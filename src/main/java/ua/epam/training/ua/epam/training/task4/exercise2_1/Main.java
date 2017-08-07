9ec6d3604e4f3b9f18255d943a5dde3a5fa59355
Admin <sorsdt@gmail.com>
2017-08-07 13:24:30 +0300

    Task 4_2.1

---
+++ src/main/java/ua/epam/training/ua/epam/training/task4/exercise2_1/Main.java
@@ -0,0 +1,28 @@
+package ua.epam.training.ua.epam.training.task4.exercise2_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException, IOException {
+        Class<?> clazz = String.class;
+        Field field = clazz.getDeclaredField("value");
+        field.setAccessible(true);
+        String originString = "test";
+        System.out.println(originString);
+        String newString = "passed";
+        field.set(originString, newString.toCharArray());
+        System.out.println(originString);
+
+        System.out.println("Please make some input:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String originInputedString = reader.readLine();
+        System.out.println(originInputedString);
+        System.out.println("Please enter new value:");
+        String newInputedString = reader.readLine();
+        field.set(originInputedString, newInputedString.toCharArray());
+        System.out.println(originInputedString);
+    }
+}
